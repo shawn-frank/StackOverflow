0419d4d6eda6b615df5723238ca9e3f2d173b7e2
shawn-frank <shawn@shawn.design>
2022-02-27 12:12:20 +0400

    initial xcode project

---
+++ CustomCollectionViewLayout/CustomCollectionViewLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewLayout
+//
+//  Created by Shawn Frank on 27/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        title = "Custom header"
+        view.backgroundColor = .white
+    }
+}
+

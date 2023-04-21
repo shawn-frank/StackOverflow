d7f0903b0484bad1ba4fd1a79af35012a919e47a
Shawn Frank <shawn@shawn.design>
2023-04-21 13:29:53 +1000

    implement cell size calculation

--- CollectionViewGrid/CollectionViewGrid/ViewController.swift
+++ CollectionViewGrid/CollectionViewGrid/ViewController.swift
@@ -15,11 +15,15 @@ class ViewController: UIViewController {
     private let minRowCount = 1
     private let maxRowCount = 5
     
+    private let cellSpacing: CGFloat = 1
+    
     private var currentColumnCount = 1
     private var currentRowCount = 1
     
     // We only want the slider value to reflect whole numbers
     private let step: Float = 1
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     @IBAction func columnSliderDidChange(_ sender: UISlider) {
         currentColumnCount = Int(round(sender.value / step) * step)
@@ -30,13 +34,21 @@ class ViewController: UIViewController {
         currentRowCount = Int(round(sender.value / step) * step)
     }
     
-    
+    private func getCellSize() -> CGSize {
+        let totalHorizontalSpacing = CGFloat(currentColumnCount - 1) * cellSpacing
+        
+        let cellWidth = (collectionView.bounds.width - totalHorizontalSpacing) / CGFloat(currentColumnCount)
+        
+        let cellHeight = cellWidth
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return currentRowCount * currentColumnCount
     }
     
     func collectionView(_ collectionView: UICollectionView,
@@ -48,6 +60,12 @@ extension ViewController: UICollectionViewDataSource {
         
         cell.cellButton.setTitle("Button \(indexPath.item + 1)", for: .normal)
         return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return getCellSize()
     }
 }
 
